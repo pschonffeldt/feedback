422af4e6d27481344f06e14682153fbc03f0985f
Pablo S <cineschonffeldt@gmail.com>
2025-06-17 10:44:37 -0500

    Add new spinner component and refactor fetch with async syntax

---
+++ src/components/FeedbackItem.tsx
@@ -0,0 +1,32 @@
+import { TriangleUpIcon } from "@radix-ui/react-icons";
+
+type FeedbackItem = {
+  upvoteCount: number;
+  badgeLetter: string;
+  comanyName: string;
+  text: string;
+  daysAgo: number;
+};
+
+type FeedbackItemProps = {
+  feedbackItem: FeedbackItem;
+};
+
+export default function FeedbackItem({ feedbackItem }: FeedbackItemProps) {
+  return (
+    <li className="feedback">
+      <button>
+        <TriangleUpIcon />
+        <span>{feedbackItem.upvoteCount}</span>
+      </button>
+      <div>
+        <p>{feedbackItem.badgeLetter}</p>
+      </div>
+      <div>
+        <p>{feedbackItem.comanyName}</p>
+        <p>{feedbackItem.text}</p>
+      </div>
+      <p>{feedbackItem.daysAgo}d</p>
+    </li>
+  );
+}
